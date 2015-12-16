9850d75d21b2f2a638d0d9405b02a5e974282646
Torsten Ruger <torsten@villataika.fi>
2015-12-16 16:26:51 +0200

    remove deprication warning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,11 @@ SalesClerk::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
   config.assets.css_compressor = :sass
   config.assets.compile = false
   config.assets.digest = true
-  
+
   # Specifies the header that your server uses for sending files.
   config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
 
