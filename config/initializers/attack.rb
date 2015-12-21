30d8137de8766a9d2673c47f67c0318c58bb3337
Torsten Ruger <torsten@villataika.fi>
2015-12-21 18:41:05 +0100

    readd attack

--- config/initializers/attack.rb
+++ config/initializers/attack.rb
@@ -1,16 +1,21 @@
 # Block requests for php or cgi, jps and what else the people throw at us
 Rack::Attack.blacklist('block admin probes') do |req|
   # Request are blocked if the return value is truthy
+  reject = false
   ["php" , "jsp" , "cgi", "asp", "cfm," "proxy.txt", "soapCaller", "Win32" , "HNAP1" , "w00tw00t",
-    "pma" , "mysql" ,"msd" , "MySQL" , "jmx-console" , "ervlet" , "xml" , "cart" ,"install", "webdav"].each do |no|
-    return true if  req.path.include?(no)
+    "pma" , "mysql" ,"msd" , "MySQL" , "jmx-console" , "ervlet" , "xml" , "cart" ,"install",
+    "webdav"].each do |no|
+    if req.path.include?(no)
+      reject = true
+      break
+    end
   end
-  false
+  reject
 end
 
 # Block requests from people clearly out to break servers
 Rack::Attack.blacklist('block admin ips') do |req|
-  [ "79.143.82.69" , "85.76.99.50", "127.0.0.1"].include? req.ip
+  [ "79.143.82.69" , "85.76.99.50" ].include? req.ip
 end
 
 # Throttle requests to 5 requests per second per ip
