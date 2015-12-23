7e8de8ac6377b8bc6865c64990eaccd47a4683d8
Torsten Ruger <torsten@villataika.fi>
2015-12-24 00:09:50 +0200

    supress pesty connect errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActionController::RoutingError, with: :error_404
+
   def error_404
     render status: 404 , :text => "move along"
   end
